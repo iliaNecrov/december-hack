2898ec4e7db9685280be55c2e606bd2ddd1f1eaa
iliaNecrov <necrofanhere@gmail.com>
2023-12-09 17:53:06 +0300

    новости теперь можно доставать отдельно списком для раздела на UI

--- src/chatbot/intents/__init__.py
+++ src/chatbot/intents/__init__.py
@@ -1,3 +1,6 @@
 from .intent_classifier import IntentClassifier
+from .google_intent.google_news import GoogleNewsIntent
+from .database_intent.algo_database import DatabaseIntent
+from .securities_intent.securities import SecuritiesIntent
 
-__all__ = ["IntentClassifier"]
+__all__ = ["IntentClassifier", "GoogleNewsIntent", "DatabaseIntent", "SecuritiesIntent"]
--- src/chatbot/intents/google_intent/google_news.py
+++ src/chatbot/intents/google_intent/google_news.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from urllib.parse import quote
-from typing import Tuple
+from typing import Tuple, List, Dict
 from gpt_api import GPT_API as gpt
 
 
@@ -52,7 +52,8 @@ class GoogleNewsIntent:
         )
 
         return prompt
-    
+
+
     def get_news_info(self, message: str) -> dict:
         """
         Составить промпт для парсинга, а после получить спаршенную информацию в json формате
@@ -61,24 +62,11 @@ class GoogleNewsIntent:
         prompt = self.get_prompt(message)
         return gpt.get_response(prompt, json_output=True)
 
-    def get_news(self, company: str, start_date: str, end_date: str) -> str:
+    @staticmethod
+    def __string_output(soup) -> str:
         """
-        Получить новости по API
+        Конвертируем новости в строку
         """
-        # Исправить данные
-        company, start_date, end_date = self._preprocess_data(company, start_date, end_date)
-
-        # Заменить новости в нужный формат для отправки URL
-        query_url = quote(company)
-        
-        # Задать URL со всеми параметрами запроса
-        url = f"https://www.google.com/search?start=0&q={query_url}&tbm=nws&tbs=cdr:1,cd_min:{start_date},cd_max:{end_date},sbd:1&lr=lang_ru"
-        
-        response = requests.get(url, headers=self.headers)
-
-        soup = BeautifulSoup(response.content, "html.parser")
-        news_results = []
-
         news = ""
         for el in soup.select("div.SoaBEf")[:5]: # парсим html, берем первые 5 новости
             title = "Заголовок: {}".format(el.select_one("div.MBeuO").get_text().strip(" \t\n"))
@@ -86,7 +74,63 @@ class GoogleNewsIntent:
             one_news = " \n".join([title, snippet]) + "\n\n"
             # собираем все новости воедино
             news += one_news
+
         return news.strip('\n')
+    
+    @staticmethod
+    def __list_ouput(soup)-> List[Dict[str, str]]:
+        news = []
+        for el in soup.select("div.SoaBEf"):
+            news_item = {
+                "link": el.find("a")["href"],
+                "title": el.select_one("div.MBeuO").get_text(),  # if el.select_one("div.MBeuO") else "Title Not Found",
+                "snippet": el.select_one(".GI74Re").get_text(),  # if el.select_one(".GI74Re") else "Snippet Not Found",
+                "date": el.select_one(".LfVVr").get_text(),  # if el.select_one(".LfVVr") else "Date Not Found",
+                "source": el.select_one(".NUnG9d span").get_text(),
+            }
+            news.append(news_item)
+
+        return news
+    
+    @classmethod
+    def get_news(cls, company: str, start_date: str = None, end_date: str = None,
+                  news_section: bool = False) -> str:
+        """
+        Получить новости по API
+        
+        Два варианта:
+        1) Для чата - на выходе строка (new_section=False)
+        2) Для новостной секции - на выходе список из словарей (news_section=True)
+        """
+        # проверяем используется ли этот метод для news section, если нет то для чата
+        if news_section and (not start_date or not end_date):
+            # Конечная дата
+            end_date = datetime.now()
+
+            # Создаем объект timedelta на 7 дней
+            seven_days = timedelta(days=7)
+
+            # Вычитаем 7 дней из текущей даты
+            start_date = (end_date - seven_days).strftime('%m/%d/%Y')
+            end_date = end_date.strftime('%m/%d/%Y')
+
+        # Исправить данные
+        company, start_date, end_date = cls._preprocess_data(company, start_date, end_date)
+
+        # Заменить новости в нужный формат для отправки URL
+        query_url = quote(company)
+        
+        # Задать URL со всеми параметрами запроса
+        url = f"https://www.google.com/search?start=0&q={query_url}&tbm=nws&tbs=cdr:1,cd_min:{start_date},cd_max:{end_date},sbd:1&lr=lang_ru"
+        
+        response = requests.get(url, headers=cls.headers)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        if news_section:
+            return cls.__list_ouput(soup)
+        else:
+            return cls.__string_output(soup)
     
     @classmethod
     def get_result(cls, message: str) -> str:
